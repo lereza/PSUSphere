923fe55fd96a7fa30247b240e2c6b92f2ec1655c
Lereza <202180308@psu.palawan.edu.ph>
2025-04-22 00:03:26 +0800

    Update ALLOWED_HOSTS in settings

--- projectsite/wsgi.py
+++ projectsite/wsgi.py
@@ -1,14 +1,16 @@
+"""
+WSGI config for projectsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
-# path where you can find the manage.py
-path = '/home/olsens/PSUsphere/projectsite'
-if path not in sys.path:
-    sys.path.insert(0, path)
+from django.core.wsgi import get_wsgi_application # type: ignore
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'projectsite.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectsite.settings')
 
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
